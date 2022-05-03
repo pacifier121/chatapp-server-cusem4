16551a32b7512e5d45251502c0b0c8c237b2daac
pacifier <prashantrawat2mx@gmail.com>
2022-05-03 23:09:57 +0530

    Testing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -75,29 +75,32 @@ router.post('/register', async(req, res, next) => { // For registering a new use
         userData.uid = "abcd4";
         userData.contacts = [];
 
-        // Running some validations
-        if (!validator.isEmail(userData.email)) {
-            return res.status(400).send({ error: "Invalid email!" });
-        } else if (userData.password.length <= 5) {
-            return res.status(400).send({ error: "Password should be greater than 5 characters" });
-        } else if (userData.age && (userData.age <= 0 || userData.age > 200)) {
-            return res.status(400).send({ error: "Invalid age!" })
-        }
+        console.log(userData);
 
-        const existingUser = await User.findOne({ email: userData.email });
-        if (existingUser) {
-            return res.status(400).send({ error: "User with this email already exists!" });
-        }
 
-        // Hashing the password before storing into DB
-        userData.password = await bcrypt.hash(userData.password, 8);
+        // // Running some validations
+        // if (!validator.isEmail(userData.email)) {
+        //     return res.status(400).send({ error: "Invalid email!" });
+        // } else if (userData.password.length <= 5) {
+        //     return res.status(400).send({ error: "Password should be greater than 5 characters" });
+        // } else if (userData.age && (userData.age <= 0 || userData.age > 200)) {
+        //     return res.status(400).send({ error: "Invalid age!" })
+        // }
 
-        const newUser = new User(userData);
-        await newUser.save();
+        // const existingUser = await User.findOne({ email: userData.email });
+        // if (existingUser) {
+        //     return res.status(400).send({ error: "User with this email already exists!" });
+        // }
 
-        // Cleaning up unncessary data
-        userData.username = undefined;
-        userData.password = undefined;
+        // // Hashing the password before storing into DB
+        // userData.password = await bcrypt.hash(userData.password, 8);
+
+        // const newUser = new User(userData);
+        // await newUser.save();
+
+        // // Cleaning up unncessary data
+        // userData.username = undefined;
+        // userData.password = undefined;
 
         // Sending filtered data back to client side
         res.send(userData);
