b3d097b74ff438908e62421bda9a68728bd2886c
Prashant Rawat <prashantrawat2mx@gmail.com>
2022-05-14 19:44:46 +0530

    Added a Procfile

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     console.log('Error! :', err);
 })
 
-const server = app.listen(process.env.PORT, () => {
-    console.log(`Server up on port ${process.env.PORT}`);
+const server = app.listen(process.env.PORT || process.env.LOCALHOST_PORT, () => {
+    console.log(`Server up on port ${process.env.PORT || process.env.LOCALHOST_PORT}`);
 })
 
 // For configuration of websockets
